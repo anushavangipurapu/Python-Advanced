29aa815e741f7e857f5b4be383ea9ee9105226e3
Anusha <Anushavangipurapu79@gmail.com>
2026-09-08 11:40:55 +0530

    feat: implement employee urls and views

--- employees/views.py
+++ employees/views.py
@@ -1,8 +1,29 @@
 from django.http import JsonResponse
 
 
+employees_data = []
+
+
 def health_check(request):
     return JsonResponse({
         "status": "success",
         "message": "Employee Management Backend is running"
     })
+
+
+def employee_list(request):
+    return JsonResponse({
+        "status": "success",
+        "employees": employees_data
+    })
+
+
+def employee_detail(request, id):
+    for employee in employees_data:
+        if employee["id"] == id:
+            return JsonResponse(employee)
+
+    return JsonResponse({
+        "status": "error",
+        "message": "Employee not found"
+    }, status=404)
